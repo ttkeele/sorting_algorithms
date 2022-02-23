8cfbe643771ed4cbb3e8f39e00d52f37949a2e24
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-02-23 13:06:08 -0800

    need these too silly

--- sort.h
+++ sort.h
@@ -42,4 +42,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void heaped(int *array, size_t size);
+void sift_down(int *array, int low, int high, size_t size);
+void heap_sort(int *array, size_t size);
+void swap(int *array, int i, int j);
 #endif /* SORT_H */

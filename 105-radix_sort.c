b95a93018f8b08e6ef8ecc702fdf4390bc21e50f
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-02 11:18:42 -0600

    radix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,101 +1,42 @@
-#include <stdlib.h>
 #include "sort.h"
+
 /**
- * csort2 - auxiliary function of radix sort
- *
- * @array: array of data to be sorted
- * @buff: malloc buffer
- * @size: size of data
- * @lsd: Less significant digit
- *
- * Return: No Return
- */
-void csort2(int *array, int **buff, int size, int lsd)
-{
-	int i, j, csize = 10, num;
-	int carr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	int carr2[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-	for (i = 0; i < size; i++)
-	{
-		num = array[i];
-		for (j = 0; j < lsd; j++)
-			if (j > 0)
-				num = num / 10;
-		num = num % 10;
-		buff[num][carr[num]] = array[i];
-		carr[num] += 1;
-	}
-
-	for (i = 0, j = 0; i < csize; i++)
-	{
-		while (carr[i] > 0)
-		{
-			array[j] = buff[i][carr2[i]];
-			carr2[i] += 1, carr[i] -= 1;
-			j++;
-		}
-	}
-
-	print_array(array, size);
-}
-/**
- * csort - auxiliary function of radix sort
- *
- * @array: array of data to be sorted
- * @size: size of data
- * @lsd: Less significant digit
- *
- * Return: No Return
- */
-void csort(int *array, int size, int lsd)
-{
-	int carr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	int i, j, num, csize = 10, **buff;
-
-	for (i = 0; i < size; i++)
-	{
-		num = array[i];
-		for (j = 0; j < lsd; j++)
-			if (j > 0)
-				num = num / 10;
-		num = num % 10;
-		carr[num] += 1;
-	}
-
-	if (carr[0] == size)
-		return;
-
-	buff = malloc(sizeof(int *) * 10);
-	if (!buff)
-		return;
-
-	for (i = 0; i < csize; i++)
-		if (carr[i] != 0)
-			buff[i] = malloc(sizeof(int) * carr[i]);
-
-
-	csort2(array, buff, size, lsd);
-
-	csort(array, size, lsd + 1);
-
-	for (i = 0; i < csize; i++)
-		if (carr[i] > 0)
-			free(buff[i]);
-	free(buff);
-}
-/**
- * radix_sort - sorts an array of integers in ascending order using the Radix
- * sort algorithm
- *
- * @array: array of data to be sorted
- * @size: size of data
- *
- * Return: No Return
+ * radix_sort - sort using a radix algorithm
+ * @array: the array that we've heaped
+ * @size: the size of array
  */
 void radix_sort(int *array, size_t size)
 {
-	if (size < 2)
+	size_t places, digit, div, i, j, total = 0, base = 10;
+	int *swap_arr;
+	/* no need to sort cases */
+	if (!array || size < 2)
 		return;
-	csort(array, size, 1);
+	/* malloc swap_arr for place holders */
+	swap_arr = malloc(sizeof(int) * size);
+	if (!swap_arr)
+		return;
+	/* find the most significant digits in the array */
+	for (i = 0; i < size; ++i)
+	{
+		for (div = 1, places = 0; array[i] / div; div *= 10)
+			++places;
+		if (places > total)
+			total = places;
+	}
+	/* sort based on significant digits, for each sig digit */
+	for (div = 1; total; --total, div *= 10)
+	{
+		/* copy the current state of the array to our swap array */
+		for (i = 0; i < size; ++i)
+			swap_arr[i] = array[i];
+		/* sort based on current values from 0 to base */
+		for (digit = 0, j = 0; digit * div < base * div && j < size; ++digit)
+			/* compare each value to the current digit */
+			for (i = 0; i < size; ++i)
+				if ((swap_arr[i] % (div * base)) / div == digit)
+					array[j++] = swap_arr[i];
+		print_array(array, size);
+	}
+	free(swap_arr);
 }
